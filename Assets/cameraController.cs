0dc322949d6605b47322548c548fb939bfe19c6a
Guy Collins <gwc593@gmail.com>
2020-04-01 14:10:45 +0100

    added files

---
+++ Assets/cameraController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+    public static GameObject _camera;
+    public float cameraSpeed = 0.5f;
+
+    Vector3 relFwd;
+    Vector3 relRight;
+
+ 
+    public Transform defaultTarget;
+
+    public Transform target;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        _camera = gameObject;
+    }
+    void Start()
+    {
+        target = defaultTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+        relFwd = (target.position - transform.position).normalized;
+        relRight = Vector3.Cross(Vector3.up, relFwd).normalized;
+        handleKeyboardInput();
+    }
+
+    public void handleKeyboardInput()
+    {
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            transform.position += relFwd * cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad5))
+        {
+            transform.position += relFwd * -cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            transform.position += relRight * -cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            transform.position += relRight * cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad9))
+        {
+            transform.position += Vector3.up * cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Keypad7))
+        {
+            transform.position += Vector3.up * -cameraSpeed * Time.deltaTime;
+        }
+    }
+
+    
+}
---
+++ Assets/craneController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class craneController : MonoBehaviour
+{
+
+    public float craneSpeed = 0.5f;
+    public float truckSpeed = 0.5f;
+    public float hookSpeed = 0.5f;
+    public GameObject hook;
+    public GameObject truck;
+    Transform camera;
+    private void Start()
+    {
+        camera = cameraController._camera.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleKeyboardInput();
+    }
+
+    private void handleKeyboardInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveForward();
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveBackward();
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            hookDown();
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            hookUp();
+        }
+    }
+
+    public void moveForward()
+    {
+        transform.position = transform.position + Vector3.forward * craneSpeed * Time.deltaTime ;
+        camera.position +=  Vector3.forward * craneSpeed * Time.deltaTime ;
+    }
+
+    public void moveBackward()
+    {
+        transform.position = transform.position + Vector3.forward * -craneSpeed * Time.deltaTime;
+        camera.position += Vector3.forward * -craneSpeed * Time.deltaTime;
+    }
+
+    public void moveRight()
+    {
+        truck.transform.position = truck.transform.position + Vector3.right * truckSpeed * Time.deltaTime;
+        camera.position += Vector3.right * truckSpeed * Time.deltaTime;
+    }
+
+    public void moveLeft()
+    {
+        truck.transform.position = truck.transform.position + Vector3.right * -truckSpeed * Time.deltaTime;
+        camera.position += Vector3.right * -truckSpeed * Time.deltaTime;
+    }
+
+    public void hookUp()
+    {
+        hook.transform.position = hook.transform.position + Vector3.up * hookSpeed * Time.deltaTime;
+        camera.position += Vector3.up * hookSpeed * Time.deltaTime;
+    }
+
+    public void hookDown()
+    {
+        hook.transform.position = hook.transform.position + Vector3.up * -hookSpeed * Time.deltaTime;
+        camera.position += Vector3.up * -hookSpeed * Time.deltaTime;
+    }
+}
+
