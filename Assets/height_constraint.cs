0e3d71d79098d4be3ea2e7d2e2bc007ecc51868d
stresiden <stresiden@gmail.com>
2020-04-03 16:05:24 +0100

    Working chain model

--- Assets/height_constraint.cs
+++ Assets/height_constraint.cs
@@ -6,20 +6,35 @@ public class height_constraint : MonoBehaviour
 {
     public GameObject connGO;
     private Vector3 vector;
+    private Vector3 velocity;
+    private Vector3 velocityconn;
+    private Vector3 oldpos;
+    private Vector3 oldconnpos;
+    public float velfactor;
+    public float gravfactor;
     public float offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        oldpos = this.transform.position;
+        oldconnpos = connGO.transform.position;
+
+        this.transform.position -= new Vector3(0f, gravfactor, 0);
+
+        this.transform.position += (velocity + velocityconn) * velfactor * Time.fixedDeltaTime;
         vector = connGO.transform.position - this.transform.position;
-        if(vector.magnitude > offset)
+        if(vector.magnitude > 0f)
         {
-            this.transform.position += vector.normalized * (vector.magnitude - offset);
+            this.transform.position += vector.normalized * (vector.magnitude - (offset));
         }
-    }
+
+        velocity = (this.transform.position - oldpos) / Time.fixedDeltaTime;
+        velocityconn = (connGO.transform.position - oldconnpos) / Time.fixedDeltaTime;
+    }   
 }
