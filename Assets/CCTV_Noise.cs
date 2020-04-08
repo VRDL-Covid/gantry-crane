b521db0e46256bdc9c68093502fd2fa01a6eb224
Guy Collins <gwc593@gmail.com>
2020-04-08 13:21:14 +0100

    Added:
    Basic panel to place CCTV controls on
    
    Noise shader to add effects to the monitor when the CCTV camera is moved,
        - This is a proof of concept to show how faulty equipment may be simulated within.

---
+++ Assets/CCTV_Noise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV_Noise : MonoBehaviour
+{
+
+    Material mat;
+    public analogueControllerBehaviour controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+    }
+
+   
+    // Update is called once per frame
+    void Update()
+    {
+        mat.SetFloat("strength", Mathf.Abs(Mathf.Max(controller.outputX, controller.outputY)));
+    }
+}
